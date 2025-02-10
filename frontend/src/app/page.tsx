f9cd94552ebe1564faf5f13a8f3414e3baab29ef
Pedro Jefferson <pedrojefferson.dev@gmail.com>
2025-02-09 22:53:49 -0300

    add home

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,101 +1,54 @@
 import Image from "next/image";
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
+import {BookMarkedIcon, ClipboardPlusIcon, EyeIcon, ListTodoIcon, ShoppingCart, Table2Icon} from "lucide-react";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+import React from "react";
+import Link from "next/link";
+import AppBar from "@/components/app-bar";
+
+
+const menuItems = [
+    { icon: ClipboardPlusIcon, title: "Criar nova solicitação", description: "Adicione uma nova solicitação de aproveitamento de horas", link: "/requests/new" },
+    { icon: EyeIcon, title: "Minhas Solicitações", description: "Acompanhe todas as solicitações cadastradas", link: "/requests/my" },
+    { icon: ListTodoIcon, title: "Solicitações", description: "Gestão de solicitações", link: "/requests/all" },
+    { icon: BookMarkedIcon, title: "Cursos", description: "Gestão de cursos", link: "/" },
+    { icon: Table2Icon, title: "Baremas", description: "Gestão de baremas", link: "/" },
+];
+
+const MenuItemCard = ({ icon: Icon, title, description, link }: any) => {
+    return (
+        <Link href={link}>
+        <Card className="w-96 p-4 flex flex-row items-center shadow-md">
+            <CardHeader className="pr-4">
+                <Icon className="w-16 h-16 text-primary" />
+            </CardHeader>
+            <CardContent className="flex flex-col">
+                <CardTitle className="text-lg font-semibold">{title}</CardTitle>
+                <p className="text-sm text-gray-500">{description}</p>
+            </CardContent>
+        </Card>
+        </Link>
+    );
+};
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      <>
+          <AppBar />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <div
+              className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+              <h1 className={"text-5xl font-bold font-mono"}>AproveitaUFBA</h1>
+              <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                  <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 gap-6 p-6">
+                      {menuItems.map((item, index) => (
+                          <MenuItemCard key={index} {...item} />
+                      ))}
+                  </div>
+              </main>
+
+
+          </div>
+      </>
   );
 }
